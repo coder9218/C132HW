21a02a8910861082cc09701e07989c553f7d3b34
Red Bull Racing Honda <80843737+coder9218@users.noreply.github.com>
2021-09-20 21:59:32 +0530

    Add files via upload

---
+++ tv.js
@@ -0,0 +1,27 @@
+status ="";
+
+function preload(){
+    img= loadImage('TV.png');
+}
+
+function setup()
+{
+    canvas = createCanvas(380, 380);
+    canvas.center();
+    objectDetector= ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+} 
+
+function modelLoaded()
+{
+    console.log("Model Loaded");
+    status = true;
+}
+
+function gotResult(error,results){
+    if(error){
+        console.log(error);
+    } else{
+    console.log(results);
+    objects = results;
+}}    
